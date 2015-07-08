9479a1e1675432aec110528ab94656f06bd8a032
Michelle Golle <michelleg54@gmail.com>
2015-07-07 20:53:20 -0600

    adding ideas js refactored

--- app/assets/javascripts/ideabox.js
+++ app/assets/javascripts/ideabox.js
@@ -1,26 +1,61 @@
 $(document).ready(function() {
-  bindListeners();
+  appendIdea();
+  fetchIdeas();
 });
 
-bindListeners = function() {
-  $('#newIdea').on('click', function(event) {
-    event.preventDefault();
-    createEvent();
-  });
-};
+function fetchIdeas() {
+  $.ajax({
+    type: "GET",
+    url: "api/v1/ideas",
+    success: function(ideas) {
+      $.each(ideas, function(index, idea) {
+        renderIdea(idea);
+      });
+    }
+  })
+}
 
-createEvent = function() {
+appendIdea = function() {
+  $('#newIdea').on('click', function(event) {
+    createIdea();
+  });
+}
+
+function renderIdea(idea) {
+  $("#ideas").append(
+  "<div class='idea' data-id=" +
+  idea.id +
+  "><h6>Title: " +
+   idea.title +
+   "</h6><p>Body: " +
+   idea.body +
+   "</p>" +
+   '<div class="buttons">' +
+   '<button class="edit">Edit</button>' +
+   '<button class="delete">Delete</button>' +
+   '<form class="edit-idea-form">' +
+   '<label>Title</label>' +
+   '<input type="text" placeholder="Title" class="idea-title">' +
+   '<label>Body</label>' +
+   '<input type="text" placeholder="Body" class="idea-body">' +
+   '<input type="submit" class="update" value="Update Idea">' +
+   '</form>' +
+   '</div>' +
+   '</div>'
+   )
+}
+
+createIdea = function() {
   var $title = $('#newIdeaTitle').val();
   var $body = $('#newIdeaBody').val();
-
   $.ajax({
     type: "POST",
     url: "api/v1/ideas",
-    dataType: "json",
-    data: {
-      idea: {title: $title, body: $body}
+    data: { idea: {title: $title, body: $body} },
+    sucess: function(data) {
+      appendIdea(data);
+      $('#newIdeaTitle').val('');
+      $('#newIdeaBody').val('');
     }
-  }).then(function(data) {
-    console.log(data);
   });
 }
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,20 @@ class Idea < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
   enum quality: %w(swill plausible genius)
+
+  def thumbs_up
+    if self.quality == 0
+      self.quality = 1
+    else
+      self.quality = 2
+    end
+  end
+
+  def thumbs_down
+    if quality == 2
+      quality = 1
+    else
+      quality = 0
+    end
+  end
 end
