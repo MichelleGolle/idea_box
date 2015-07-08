dbcb15504da4ae26472313de0abe8e8de679fd9d
Michelle Golle <michelleg54@gmail.com>
2015-07-07 21:41:39 -0600

    delete working and update WIP

--- app/assets/javascripts/ideabox.js
+++ app/assets/javascripts/ideabox.js
@@ -11,6 +11,9 @@ function fetchIdeas() {
       $.each(ideas, function(index, idea) {
         renderIdea(idea);
       });
+      $(".delete").on("click", deleteIdea);
+      $(".edit").on("click", editIdea);
+      $(".update").on("click", updateIdea);
     }
   })
 }
@@ -25,9 +28,9 @@ function renderIdea(idea) {
   $("#ideas").append(
   "<div class='idea' data-id=" +
   idea.id +
-  "><h6>Title: " +
+  "><h6>" +
    idea.title +
-   "</h6><p>Body: " +
+   "</h6><p>" +
    idea.body +
    "</p>" +
    '<div class="buttons">' +
@@ -58,4 +61,43 @@ createIdea = function() {
       $('#newIdeaBody').val('');
     }
   });
+}
+
+function deleteIdea() {
+    var $idea = $(this).parents('.idea');
+    var id = $idea.data('id');
+    $.ajax('api/v1/ideas/' + id, { method: "delete" }).then(function() {
+      $idea.remove();
+    });
+}
+
+function editIdea() {
+    var $idea = $(this).parents('.idea');
+    var $form = $(this).siblings('.edit-idea-form')
+    if($form.is(':hidden')) {
+      var title = $idea.find('h6').text();
+      var body = $idea.find('p').text();
+      $form.find('.idea-title').val(title);
+      $form.find('.idea-body').val(body);
+    }
+    $form.toggle();
+}
+
+function updateIdea(idea) {
+  console.log($(this))
+    var $idea = $(this).parents('.idea');
+    var id = $idea.data('id');
+    var $title = $(this).siblings('.idea-title').val();
+    var $body = $(this).siblings('.idea-body').val();
+
+    $.ajax({
+      type: "PUT",
+      url: "api/v1/ideas" + id,
+      data: {title: $title, body: $body},
+      sucess: function(idea) {
+        $idea.find('h6').text(idea.title);
+        $idea.find('p').text(idea.body);
+        $idea.find('form').hide();
+      }
+    });
 }
